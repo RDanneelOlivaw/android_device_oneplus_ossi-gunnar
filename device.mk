6c0ef6d79d4f57fa459d2ed5edd73086a0fe2c73
Daniel Rosania <daniel.rosania1638@gmail.com>
2024-08-24 00:33:17 -0400

    ossi: Move to new RFS install_symlink targets
    Change-Id: I26f6d2ec7f42e50bfd43b3763b9d9ea00d4fd098

--- device.mk
+++ device.mk
@@ -3,7 +3,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Commenting to record successful kernel build
+
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
